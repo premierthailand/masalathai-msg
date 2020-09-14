501f85a91fa61cd8e96c825ecebc46011b2783a3
premierthailand <70932621+premierthailand@users.noreply.github.com>
2020-09-14 15:51:28 +0700

    msg upload img

---
+++ Connections/dbcon.php
@@ -0,0 +1,14 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_dbcon = "localhost";
+$database_dbcon = "masalath_msg";
+$username_dbcon = "root";
+$password_dbcon = "";
+$dbcon = mysql_pconnect($hostname_dbcon, $username_dbcon, $password_dbcon) or trigger_error(mysql_error(),E_USER_ERROR); 
+mysql_query("set names utf8");
+error_reporting(E_ALL ^ E_NOTICE);
+
+
+?>
---
+++ form_upload.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+<br><br><br><br><br><br><br>
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
